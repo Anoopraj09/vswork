25dab0e6cdfbdd776a09d7edbe6e538d808f1550
Anoopraj7500 <anooprajput331@gmail.com>
2024-05-30 20:07:28 +0530

    updated.py

--- annual_sales_performance.py
+++ annual_sales_performance.py
@@ -1,3 +1,50 @@
+
+<<<<<<< HEAD
+Readme created.
+import os
+
+import pandas as pd
+import numpy as pd
+import mysql.connector 
+from dotenv import load_dotenv
+
+load_dotenv()
+host = os.getenv('HOST')
+username = os.getenv('USER')
+password = os.getenv('PASWORD')
+
+connection = mysql.connector.connect(host=host,
+                                     user=username,
+                                     password=password,
+                                     database='swiftmarket')
+
+cursor = connection.cursor()
+
+
+
+
+
+def queryToDataFrame(query):
+
+    cursor. execute(query)
+    rows = cursor.fetchall()
+    df = pd.DataFrame(data=rows,columns=cursor.column_names)
+    return df
+
+def shoeTables():
+query = """SHOW TABLES;"""
+cursor.execute(query)
+rows = cursor.fetchall()
+df = pd.DataFrame(data=rows,columns=cursor.column_names)
+retrun df
+
+def describeTable(tablename):
+query = f"""describe {tablename};"""
+cursor.execute(query)
+rows = cursor.fetchall()
+df = pd.DataFrame(data=rows,columns=cursor_names)
+return df
+
 <html>
 <head><meta charset="utf-8" /></head>
 <body>
